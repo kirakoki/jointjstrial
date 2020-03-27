a394c2df577621ae80a31132efb637b818b37eb0
Kemal Akyigit <kemal.akyigit@hotmail.com>
2020-03-27 03:01:42 +0000

    initial-setup

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,112 @@
+<template>
+  <div class="container">
+    <div id="paper"></div>
+    <button class="danger" @click="addShape">Add Shape</button>
+    <button class="warning" @click="addShape">Remove Shape</button>
+  </div>
+</template>
+
+<script>
+// import * as _ from 'lodash';
+// import * as $ from 'backbone';
+const joint = require("jointjs/dist/joint.js");
+
+import "jointjs/dist/joint.css";
+
+export default {
+  data: {
+    graph: null,
+    cellMap: {}, 
+    rootCell: null,
+    selectedCell: null
+  },
+
+  created() {
+    const vm = this;
+    var graphInstance = new joint.dia.Graph();
+    vm.graph = graphInstance;
+  },
+  mounted() {
+    var paper = new joint.dia.Paper({
+      el: $("#paper"),
+      width: 1200,
+      height: 600,
+      gridSize: 1,
+      model: this.graph,
+      perpendicularLinks: true,
+      restrictTranslate: true
+    });
+    paper.on('cell:pointerdown', (cellView, evt, x, y) => {
+      if (this.cellMap[cellView.model.id]) {
+        this.activeCell = this.cellMap[cellView.model.id];
+      }
+      //this.activeCell.cell.remove()
+    }
+);
+  },
+  methods: {
+    link(source, target, breakpoints) {
+      var cell = new joint.shapes.org.Arrow({
+        source: { id: source.id },
+        target: { id: target.id },
+        vertices: breakpoints,
+        attrs: {
+          ".connection": {
+            fill: "none",
+            "stroke-linejoin": "round",
+            "stroke-width": "2",
+            stroke: "#4b4a67"
+          }
+        }
+      });
+      this.graph.addCell(cell);
+      return cell;
+    },
+    addShape() {
+      // this.objects in empty oldugu, 
+      // starting position hardcoded
+      // this.objects den son itemin x, y koordinatlarini oku
+      // yeni cell in pozisyonunu hesapla
+      // previousX + CELL_WIDTH + OFFSET
+      const X_OFFSET = 200;
+      const Y_OFFSET = 0;
+      let x = 50;
+      let y = 200;
+      if (this.activeCell) {
+        const { x: previousX, y: previousY } = this.activeCell.cell.attributes.position;
+        x = previousX + X_OFFSET;
+        y = previousY + Y_OFFSET;
+      }
+
+      var cell = new joint.shapes.basic.Rect();
+      cell.resize(100, 200);
+      cell.position(x, y);
+      cell.attr("root/title", "joint.shapes.standard.Rectangle");
+      cell.attr("label/text", "Rectangle");
+      cell.attr("body/fill", "lightblue");
+      cell.addTo(this.graph);
+      //cell.attr['body']= red
+
+      this.graph.addCell(cell);
+      let newCell = { cell, linkForwards: null, linkUpwards: null, linkDownwards: null};
+      this.cellMap = {
+        [cell.id]: newCell,
+        ...this.cellMap
+      }
+      const previousCell = this.activeCell;
+      if (previousCell) {
+        // determine which direction to link
+        previousCell.linkForwards = newCell;
+        this.link(previousCell.cell, cell);
+      }
+
+      if (!this.activeCell) {
+        this.rootCell = newCell;
+      }
+      console.log(this.rootCell);
+      this.activeCell = newCell;
+      return cell;
+    }
+  }
+};
+</script>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <!-- <link rel="stylesheet" type="text/css" href="node_modules/jointjs/dist/joint.css" /> -->
+    <link src="css/app.css" />
+</head>
+<body>
+    <div id="app">
+        <example-component/>
+    </div>
+    <!-- content -->
+ 
+
+    <!-- dependencies --><!-- 
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/lodash.js/3.10.1/lodash.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.3.3/backbone.js"></script> -->
+    <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jointjs/2.1.0/joint.js"></script> -->
+
+    <!-- code -->
+    <script src="js/app.js"></script>
+
+</body>
+</html>
